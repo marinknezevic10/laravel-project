9ffff0b74c48af91a03903e14162bec1f866a83e
Marin <marin.knezevic1et2@gmail.com>
2021-05-04 13:55:10 +0200

    User/posts relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,8 @@ class PostController extends Controller
 
     public function store(Request $request )
     {
-        dd('ok');
+        $this->validate($request,[
+            'body'=>'required'
+        ]);
     }
 }
