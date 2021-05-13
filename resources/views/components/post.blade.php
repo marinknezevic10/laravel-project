7c2b33a93da5ccb87927d4cb290ac7a02ac73c26
marinknezevic10 <73716434+marinknezevic10@users.noreply.github.com>
2021-05-13 14:21:47 +0200

    blade components, tyding the code and user profile

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,3 +1,39 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+                @props(['post'=>$post])               
+                <div class="mb-4">
+                    <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a><!--$post->user->name
+                ispisujem vezu user-post koju sam kreirao u post modelu-->
+                    <span class="text-gray-600text-sm">{{ $post->created_at->diffForHumans() }}</span>
+
+                    <p class="mb-2">{{ $post->body }}</p>
+   
+                    @can('delete', $post)                
+                        <form action="{{ route('posts.destroy', $post) }}" method="post">
+                            @csrf
+                            @method('DELETE')  
+                            <button type="submit" class="text-blue-500">Delete</button>
+                        </form>
+                    @endcan  
+                 
+                    <div class="flex items-center">
+                        @auth
+                        <!--if the user already liked the post-->
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes',$post) }}" method="post" 
+                                class="mr-1">
+                                @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes',$post) }}" method="post" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+                     
+                        @endauth
+
+                    <span> {{ $post->likes->count() }} {{ Str::plural('like', 
+                    $post->likes->count()) }}</span><!-- implementing likes onto page-->
+                    </div>
+                </div>
