447b93bdcd563bf2469ce58f10d0b4146c3f0ee0
marinknezevic10 <73716434+marinknezevic10@users.noreply.github.com>
2021-05-13 14:22:42 +0200

    blade components, tyding the code and user profile

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('sadrzaj')
+
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
             <form action="{{ route('posts') }}" method="post" class="mb-4">
             @csrf
+            @auth
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100
@@ -17,50 +19,17 @@
                         </div>
                     @enderror
                 </div>
-
+                
                 <div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded
-                    font-medium">Objavi</button>
+                    font-medium">Objavi</button> 
                 </div>
+                @endauth
             </form>
 
             @if($posts->count())
                 @foreach($posts as $post)
-                <div class="mb-4">
-                    <a href="" class="font-bold">{{ $post->user->name }}</a><!--$post->user->name
-                ispisujem vezu user-post koju sam kreirako u post modelu-->
-                    <span class="text-gray-600text-sm">{{ $post->created_at->diffForHumans() }}</span>
-
-                    <p class="mb-2">{{ $post->body }}</p>
-                    
-                    @can('delete', $post)                
-                        <form action="{{ route('posts.destroy', $post) }}" method="post">
-                            @csrf
-                            @method('DELETE')  
-                            <button type="submit" class="text-blue-500">Delete</button>
-                        </form>
-                    @endcan  
-                    
-                    <div class="flex items-center">
-                    <!--if the user already liked the post-->
-                        @if (!$post->likedBy(auth()->user()))
-                            <form action="{{ route('posts.likes',$post) }}" method="post" 
-                            class="mr-1">
-                            @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                        @else
-                            <form action="{{ route('posts.likes',$post) }}" method="post" class="mr-1">
-                            @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
-                        @endif
-
-                    <span> {{ $post->likes->count() }} {{ Str::plural('like', 
-                    $post->likes->count()) }}</span><!-- implementing likes onto page-->
-                    </div>
-                </div>
+                <x-post :post="$post" />
                 @endforeach
                 
                 {{ $posts->links() }}<!-- pagination-->
